10a5bc82af6b21cda1164de5b3a04a84da7445cb
ewinchen <chenyongp@esquel.com>
2018-11-29 09:23:31 +0800

    feat: add sign module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import { SignInComponent } from './view/sign/sign-in/sign-in.component';
+import { SignUpComponent } from './view/sign/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: './view/main/main.module#MainModule'
+  },
+  {
+    path: 'signin',
+    component: SignInComponent
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignModule } from './sign/sign.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SignModule
   ]
 })
 export class ViewModule { }
