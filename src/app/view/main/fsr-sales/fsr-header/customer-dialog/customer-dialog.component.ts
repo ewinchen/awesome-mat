be2e59b93b5140e9e539fa081ea1933834020b20
ewinchen <chenyongp@esquel.com>
2018-12-10 12:26:44 +0800

    feat: update customer dialog

--- src/app/view/main/fsr-sales/fsr-header/customer-dialog/customer-dialog.component.ts
+++ src/app/view/main/fsr-sales/fsr-header/customer-dialog/customer-dialog.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatAutocompleteSelectedEvent } from '@angular/material';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, mergeMap, debounceTime } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { map, mergeMap, debounceTime, tap, distinctUntilChanged, switchMap, skip } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
 export interface CustomerDialogData {
@@ -19,28 +19,73 @@ export interface CustomerDialogData {
   styleUrls: ['./customer-dialog.component.scss']
 })
 export class CustomerDialogComponent implements OnInit {
-  myControl = new FormControl();
-  filteredOptions: Observable<string[]>;
+  customerCodeControl = new FormControl(this.data.customerCode);
+  brandControl = new FormControl(this.data.brand);
+  customerLabelControl = new FormControl(this.data.customerLabel);
 
+  thing = {
+    name: 'Edwin',
+    capacity: 'Strong'
+  }
+
+  testControl = new FormControl(this.thing.name);
+
+  customerCodes: Array<any>;
+  brands: Array<any>;
+  customerLabels: Array<any>;
 
   constructor(public dialogRef: MatDialogRef<CustomerDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: CustomerDialogData,
     private http: HttpClient) { }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        debounceTime(500),
-        mergeMap((value) => {
-          return this.http.post<any>('http://localhost:10010/fetchTableFind/genCustomerContains?isMultiple=true&pageSize=200', {
-            customer_cd: value
-          });
-        }),
-        map(res => res.map(item => item.customerCd)),
-      );
+    this.customerCodeControl.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(() => {
+        this.data.customerName = '';
+        this.data.brand = '';
+        this.data.customerLabel = '';
+      }),
+      switchMap((value) => {
+        const getCustomerCodes$ = this.http.post<any>('http://localhost:10010/fetchTableFind/genCustomerContains?isMultiple=true&pageSize=200', {
+          customer_cd: value
+        });
+        const getBrands$ = this.http.post<any>('http://localhost:10010/getBrandByCustomerCode', {
+          customer_cd: value
+        });
+        const getCustomerLabels$ = this.http.post<any>('http://localhost:10010/fetchTableFind/genBrandLabelContains?isMultiple=true&pageSize=200', {
+          customer_cd: value,
+        });
+
+        return forkJoin(getCustomerCodes$, getBrands$, getCustomerLabels$);
+      }),
+    ).subscribe(([customerCodes, brands, customerLabels]) => {
+      this.customerCodes = customerCodes;
+      this.brands = brands;
+      this.customerLabels = customerLabels;
+    });
+
   }
 
   onCancel() {
     this.dialogRef.close();
+  }
+
+  onCustomerCodeChanged(e) {
+    console.log(e);
+    this.data.customerName = this.customerCodes.find(item => item.customerCd === e.target.value).name;
+  }
+
+  onTest() {
+    console.log(this.thing);
+  }
+
+  onSetTest() {
+    this.testControl.setValue('Whhhhhhhat!!!! Super Edwin!!!???!!!')
+  }
+
+  onTestChange(e) {
+    console.log(e);
   }
 }
